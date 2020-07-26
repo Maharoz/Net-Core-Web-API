6b54ce86fdd0173ddc0fe21331116e3323662f6c
maharoz <mahruj66@gmail.com>
2020-07-26 13:39:11 +0600

    Global error handling added

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -30,26 +30,11 @@ namespace CompanyEmployees.Controllers
         [HttpGet]
         public IActionResult GetCompanies()
         {
-            try
-            {
                 var companies = _repository.Company.GetAllCompanies(trackChanges: false);
 
-                //var companiesDto = companies.Select(c => new CompanyDto
-                //{
-                //    Id = c.Id,
-                //    Name = c.Name,
-                //    FullAddress = string.Join(' ',c.Address,c.Country)
-                //}).ToList();
                 var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
                 
                 return Ok(companiesDto);
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
-                return StatusCode(500, "Internal server error");
-
-            }
         }
     }
 }
---
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Entities.ErrorModel
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString()=> JsonConvert.SerializeObject(this);
+    }
+}
