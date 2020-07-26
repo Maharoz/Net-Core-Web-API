7e07b239345f6cf072d873996977ee44a02fabc2
maharoz <mahruj66@gmail.com>
2020-07-26 14:22:46 +0600

    Content negotiation demonstrated

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -40,7 +40,12 @@ namespace CompanyEmployees
             services.ConfigureLoggerService();
             services.ConfigureRepositoryManager();
             services.AddAutoMapper(typeof(Startup));
-            services.AddControllers();
+            services.AddControllers(config=>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters()
+            .AddCustomCSVFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
