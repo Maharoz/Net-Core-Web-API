85be3e090d95bbd77121683bbb560bad58cf074c
maharoz <mahruj66@gmail.com>
2020-07-29 18:40:30 +0600

    Searching added added

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -23,6 +23,7 @@ namespace Entities.RequestFeatures
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue; 
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string SearchTerm { get; set; }
     }
 }
 
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,8 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
+            var employees =await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge).Search(employeeParameters.SearchTerm)
                 .OrderBy(e => e.Name)
                 .ToListAsync(); 
             return PagedList<Employee>.
