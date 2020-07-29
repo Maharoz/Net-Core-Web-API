ef0ecfcc9fd6c7faac338389ee6c6d1033f43129
maharoz <mahruj66@gmail.com>
2020-07-29 18:28:56 +0600

    Filtering added

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -30,6 +30,7 @@ namespace CompanyEmployees.Controllers
         [HttpGet]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery] EmployeeParameters employeeParameters)
         {
+            if (!employeeParameters.ValidAgeRange) return BadRequest("Max age can't be less than min age.");
             var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -19,8 +19,10 @@ namespace Entities.RequestFeatures
             }
         }
     }
-    public class EmployeeParameters : RequestParameters { 
-    
+    public class EmployeeParameters : RequestParameters {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue; 
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
 
